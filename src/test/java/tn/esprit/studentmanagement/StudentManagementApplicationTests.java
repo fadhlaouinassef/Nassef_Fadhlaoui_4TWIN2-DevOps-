4161697079ea349b51eba2ccf6faa88997424e7f
fadhlaouinassef <nasseffadhlaoui@gmail.com>
2025-11-26 18:11:35 +0100

    Correction

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -2,12 +2,15 @@ package tn.esprit.studentmanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
+        // Ce test vérifie simplement que le contexte Spring démarre correctement
     }
 
 }
