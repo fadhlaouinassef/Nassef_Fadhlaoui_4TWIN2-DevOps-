cb9ffe5005ee84939737d7502c58924432729855
fadhlaouinassef <nasseffadhlaoui@gmail.com>
2025-11-26 17:53:20 +0100

    Correction de StudentService

--- src/main/java/tn/esprit/studentmanagement/services/StudentService.java
+++ src/main/java/tn/esprit/studentmanagement/services/StudentService.java
@@ -1,5 +1,7 @@
 package tn.esprit.studentmanagement.services;
 
+
+
 import org.springframework.stereotype.Service;
 import tn.esprit.studentmanagement.entities.Student;
 import tn.esprit.studentmanagement.repositories.StudentRepository;
@@ -8,7 +10,13 @@ import java.util.List;
 
 @Service
 public class StudentService implements IStudentService {
-    private StudentRepository studentRepository;
+
+
+    private final StudentRepository studentRepository;
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getAllStudents() { return studentRepository.findAll(); }
     public Student getStudentById(Long id) { return studentRepository.findById(id).orElse(null); }
     public Student saveStudent(Student student) { return studentRepository.save(student); }
